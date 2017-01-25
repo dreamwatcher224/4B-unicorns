9f465589b51bc37cf34480f62ed2c78379f70f84
chickendumplings <mac8@129mac18.valleychristian.net>
2017-01-25 14:44:00 -0800

    added buttons with alphabetical letters on

--- webapp.js
+++ webapp.js
@@ -3,4 +3,4 @@
 // Random Word Chooser
 var words = ["cake", "steak", "carrot", "zucchini", "cucumber", "cookie", "tomato", "potato"];
 var randselect = words[Math.floor(Math.random() * words.length)];
-document.write(randselect);
+
