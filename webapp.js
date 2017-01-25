508ee5dc4eaa2421740b9e331c2755122c4dfaaa
James Ding <mac8@129mac18.valleychristian.net>
2017-01-25 14:44:15 -0800

    added category words and buttons

--- webapp.js
+++ webapp.js
@@ -3,8 +3,11 @@
 // Random Word Chooser
 var words = ["cake", "steak", "carrot", "zucchini", "cucumber", "cookie", "tomato", "potato"];
 var randselect = words[Math.floor(Math.random() * words.length)];
-document.write(randselect);
 
 // Categories
-var categories = [food, sports, animals,];
+var food = ["cake", "steak", "carrot", "zucchini", "cucumber", "cookie", "tomato", "potato", "bacon", "pork", "ice cream", ];
+var sports = ["baseball", "football", "soccer", "table tennis", "tennis", "badminton", "basketball", "water polo", "hockey", "swimming"];
+var animals = ["ant", "squirrel", "rabbit", "dog", "cat", "salamander", "alligator", "crocodile", "donkey", "horse", "tiger", "lion", "cheetah", "leopard", "lemur"];
+var countries = ["Argentina", "America", "China", "Japan", "Russia", "Canada", "Brazil", "Italy", "Australia", "Mexico", "Spain", "Korea", "Egypt", "Greece", "France", "Greenland", "Iceland", "England", "Portugal", "Vietnam", "India"];
+
 
