5656e096ddac73ce9579d09b96992051eb929e16
Julia Kim <mac8@129mac18.valleychristian.net>
2017-01-25 14:56:24 -0800

    Added function to check letters

--- webapp.js
+++ webapp.js
@@ -3,4 +3,20 @@
 // Random Word Chooser
 var words = ["cake", "steak", "carrot", "zucchini", "cucumber", "cookie", "tomato", "potato"];
 var randselect = words[Math.floor(Math.random() * words.length)];
-document.write(randselect);
+// Prints blank underscores
+for(var i = 0; i < randselect.length; i++) {
+	document.write("_ ");
+}
+// Function to check if letter is in word
+var bool = false;
+function check(x) {
+	for(var i = 0; i < randselect.length; i++) {
+		if(randselect.charAt(i) == x) {
+			bool = true;
+		}
+	}
+	if(bool == false) {
+		
+	}
+	//document.getElementById('wrong').innerHTML += "<br>document.write(x)";
+}
