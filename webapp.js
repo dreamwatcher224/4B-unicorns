df9f681ec2df4d264ce397f6be7c92f3e2d746bf
James Ding <mac8@129mac18.valleychristian.net>
2017-01-27 13:52:04 -0800

    Added buttons

--- webapp.js
+++ webapp.js
@@ -1,13 +1,21 @@
 // JavaScript Document
 
-// Random Word Chooser
-var words = ["cake", "steak", "carrot", "zucchini", "cucumber", "cookie", "tomato", "potato"];
-var randselect = words[Math.floor(Math.random() * words.length)];
-
 // Categories
-var food = ["cake", "steak", "carrot", "zucchini", "cucumber", "cookie", "tomato", "potato", "bacon", "pork", "ice cream", ];
+var categories = [food, sports, animals, countries];
+var food = ["cake", "steak", "carrot", "zucchini", "cucumber", "cookie", "tomato", "potato", "bacon", "pork", "ice cream"];
 var sports = ["baseball", "football", "soccer", "table tennis", "tennis", "badminton", "basketball", "water polo", "hockey", "swimming"];
 var animals = ["ant", "squirrel", "rabbit", "dog", "cat", "salamander", "alligator", "crocodile", "donkey", "horse", "tiger", "lion", "cheetah", "leopard", "lemur"];
 var countries = ["Argentina", "America", "China", "Japan", "Russia", "Canada", "Brazil", "Italy", "Australia", "Mexico", "Spain", "Korea", "Egypt", "Greece", "France", "Greenland", "Iceland", "England", "Portugal", "Vietnam", "India"];
+
+// Random Word Chooser
+var category = 
+var randselect = category[Math.floor(Math.random() * category.length)];
+
+// Category Functions
+function c(val) {
+	category = 
+}
+
+
 
 
