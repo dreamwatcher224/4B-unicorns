dc39da2be88cd7f6b3a39fb77d63292889df568d
MckCanSeeDeep <mac8@129mac18.valleychristian.net>
2017-02-17 15:25:12 -0800

    Revert "Merge branch 'master' into Version1.0"
    This reverts commit ac52b8c0f8a67d757711e5ae30c8436e58856222, reversing
    changes made to 875c75edec29debc6836d963736411d02f2fe621.

--- webapp.js
+++ webapp.js
@@ -33,6 +33,7 @@ var storage = 0;
 var index = [];
 var right = false;
 var blank = "";
+
 function v(x) {
 	var test = x.toLowerCase();
 	for(var i = 0; i < randselect.length; i++) {
@@ -41,23 +42,31 @@ function v(x) {
 			index[counter] = i;
 			counter++;
 		}
+		/*var array = document.getElementsByClassName("used button letter");
+		for(var i = 0; i < array.length; i++) {
+			array[i].style.display = "none";
+		}
+		var array2 = document.getElementsByClassName("button letter");
+		for(var i = 0; i < array2.length; i++) {
+			array2[i].style.display = "inline";
+		}*/
 	}
-
+	document.getElementById("words").innerHTML = blank;
+	//document.getElementById("words").innerHTML = "testing";
 	if(right == false) {
-		
+		$("#"+x.toLowerCase()).appendTo("#guessed");
 	} else {
 		for(var j = 0; j < index.length; j++) {
-			if(index[i] == 0) {
-				
-			} else {
-				document.write("blank: " + blank);
-				blank = blank.substr(0, (index[i])) + index[i] + blank.substr(index[i+1]);
+			if(index[j] == 0) {
+				blank = x + blank.substring(1, blank.length);
+			} else {// _ _ _ _ _ 
+				blank = blank.substring(0, 2*index[j]) + x + blank.substring(2*index[j]+1, blank.length);
 			}
 		}
+		update_display();
 	}
 	index = [];
 	counter = 0;
-
 	right = false;
 }
 
@@ -76,8 +85,12 @@ function gen() {
 	document.getElementById(test + "_used").style.display = "none";
 	document.getElementById(test).style.display = "inline";
 	
-
 }
+
+function update_display() {
+	document.getElementById("words").innerHTML = blank;
+}
+
 // Random Word Chooser
 
 var randselect = "";
@@ -103,4 +116,3 @@ function v(x) {
 }*/
 
 //JQuery
-
