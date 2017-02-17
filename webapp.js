875c75edec29debc6836d963736411d02f2fe621
MckCanSeeDeep <mac8@129mac18.valleychristian.net>
2017-02-17 15:21:57 -0800

    Fixed stuff

--- webapp.js
+++ webapp.js
@@ -2,11 +2,11 @@
 
 // Categories
 
-var categories = [food, sp	orts, animals, countries];
-var food = ["cake", "steak", "carrot", "zucchini", "cucumber", "cookie", "tomato", "potato", "bacon", "pork", "ice cream"];
-var sports = ["baseball", "football", "soccer", "table tennis", "tennis", "badminton", "basketball", "water polo", "hockey", "swimming"];
-var animals = ["ant", "squirrel", "rabbit", "dog", "cat", "salamander", "alligator", "crocodile", "donkey", "horse", "tiger", "lion", "cheetah", "leopard", "lemur"];
-var countries = ["Argentina", "America", "China", "Japan", "Russia", "Canada", "Brazil", "Italy", "Australia", "Mexico", "Spain", "Korea", "Egypt", "Greece", "France", "Greenland", "Iceland", "England", "Portugal", "Vietnam", "India"];
+var food = ["cake", "steak", "carrot", "zucchini", "cucumber", "cookie", "tomato", "potato", "bacon", "pork", "tamale", "hamburger", "fries", "salad", "sushi", "pizza", "beets", "blackberries", "avocado", "figs", "cherries", "rice", "waffle", "honey"];
+/*var sports = ["baseball", "football", "soccer", "tennis", "badminton", "basketball", "hockey", "swimming", "archery", "softball", "volleyball", "kayaking", "climbing", "boxing", "diving", "cycling", "fencing", "surfing", "wrestling", "canoeing", "skiing"];
+var animals = ["ant", "squirrel", "rabbit", "dog", "cat", "salamander", "alligator", "crocodile", "donkey", "horse", "tiger", "lion", "cheetah", "leopard", "lemur", "fox", "wolf", "bobcat", "python", kangaroo", "sheep", "gorilla", "chameleon, "deer", mole"];
+var countries = ["Argentina", "America", "China", "Japan", "Russia", "Canada", "Brazil", "Italy", "Australia", "Mexico", "Spain", "Korea", "Egypt", "Greece", "France", "Greenland", "Iceland", "England", "Portugal", "Vietnam", "India", "Netherlands"];
+var category = "";
 
 
 
@@ -73,7 +73,6 @@ function v(x) {
 function update(a) {
 	document.getElementById("words").innerHTML = a;
 }
-
 function gen() {
 	blank = "";
 	randselect = food[Math.floor(Math.random() * food.length)];
@@ -97,7 +96,6 @@ function update_display() {
 var randselect = "";
 var blank = "";
 
-document.write(blank);
 //document.getElementById("words").innerHTML = blank;
 
 
